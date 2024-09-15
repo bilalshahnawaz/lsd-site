40461a47075ff0783f731f0095eaa87535dd9cca
Bilal Shahnawaz <bilalshahnawaz@icloud.com>
2024-09-15 19:20:36 -0400

    Initialize project

---
+++ src/lib/data.ts
@@ -0,0 +1,44 @@
+import React from "react";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Durb",
+    description:
+      "Durb durb durb",
+    tags: ["Web", "Competitive", "Strategy", "Risk", "Clash Royale"],
+    // imageUrl: corpcommentImg,
+  },
+  {
+    title: "Ghosts Don't Sleep",
+    description:
+      "Ghosts dont sleep",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    // imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    // imageUrl: wordanalyticsImg,
+  },
+] as const;
